7c898b1a9876cfb87b38572ec86fd0b962e6c72c
shun04101 <nakashimashuntarou@gmail.com>
2022-01-16 14:06:46 +0000

    [Add]Top/Timeline/About/各種レイアウト

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,17 @@
 class SearchesController < ApplicationController
   
   def search
+    @posts = Post.all.order(created_at: :desc)
     @range = params[:range]
+    # binding.pry
+    if @range == "PostTags"
+      @item = "タグ"
+    else
+      @item = "タイトル"
+    end
     search = params[:search]
     @word = params[:word]
+    # binding.pry
     if @range == 'Posts'
       @post = Post.search(search, @word)
     else
