39c738cd24d551e0eea2b3595a5fef5b0f21e093
shun04101 <nakashimashuntarou@gmail.com>
2022-01-18 07:12:54 +0000

    [Add]フォロー機能/ログイン制限

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   
   def index
     @user = User.find_by(id: params[:user_id])
--- app/models/site.rb
+++ app/models/site.rb
@@ -5,5 +5,5 @@ class Site < ApplicationRecord
   validates :link, presence: true
   validates :info, presence: true
   
-  enum info: { tops: 0, bottoms: 1, accessory: 2, other: 3 }
+  enum info: { tops: 0, jacket: 1, pants: 2, skirt: 3, dress: 4, shoes: 5, goods: 6, accessory: 7, other: 8 }
 end
