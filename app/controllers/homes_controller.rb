5e73d0aa4f9fb28ba57bfa7f249fe41646d9d2d4
shun04101 <nakashimashuntarou@gmail.com>
2022-02-09 13:41:46 +0000

    [Fix]インデント等調整

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,6 @@
 class HomesController < ApplicationController
   def top
     @posts = Post.all.order(created_at: :desc)
-    # @post = Post.find_by( id: params[:user_id])
-    # @post_tags = @post.tags
   end
   
   def about
